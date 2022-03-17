bf7e22f7dfd414bee2fe98ef2ddd7f1e068a4233
FierceExplorer <89167174+FierceExplorer@users.noreply.github.com>
2022-03-17 13:26:44 -0700

    Delete matrix.py

--- matrix.py
+++
@@ -1,8 +0,0 @@
-matrix = [ [1,2,3],[4,5,6],[7,8,9] ] 
-def matrix_maker(list): 
-  for index in list: 
-    print(str(index[0]) + " " + str(index[1]) + " " + str(index[2]))
-    
-
-    
-matrix_maker(matrix)
